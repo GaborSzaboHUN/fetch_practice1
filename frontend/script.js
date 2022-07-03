0ff7e3e2352ace2239f38a83e83683c7cdd09d06
GaborSzaboHUN <66555347+GaborSzaboHUN@users.noreply.github.com>
2022-07-03 02:17:57 +0200

    search is working

--- frontend/script.js
+++ frontend/script.js
@@ -1,60 +1,11 @@
-/* 
-const sectionElement = function (title, image, photographer, details) {
+const searchElement = function () {
     return `
-        <h1>Check the space</h1>
-        <h3>${title}</h3>
-        <iframe src="${image}"></iframe>
-        <img src="${image}" alt="Image from space">
-        <p>${photographer}</p>
-        <p>${details}</p>
+    <input type="text" class="search-bar" name="country-name" required placeholder="Type a country name...">
+	<button id="search-country" type="button">Search</button>
     `
 }
 
-const nasaApiKey = "nulBNTuYQMkvZlxM1YUWVMHu72zCgpFi8EwIpBaz"
-
-console.log("load event előtt")
-
-const loadEvent = async function () {
-    const rootElement = document.getElementById("root")
-
-    console.log("load event után")
-
-    fetch(`https://api.nasa.gov/planetary/apod?api_key=${nasaApiKey}`)
-        .then(function (response) {
-            console.log(response)
-            return response.json();
-        })
-        .then(function (json) {
-            console.log(json)
-            rootElement.insertAdjacentHTML("beforeend", sectionElement(json.title, json.hdurl, json.copyright, json.explanation))
-        })
-
-
-    console.log("fetch előtt")
-
-    setTimeout(function () {
-        console.log("fetch előtt setTimout-tal")
-    }, 5000);
-
-    let response = await fetch(`https://api.nasa.gov/planetary/apod?api_key=${nasaApiKey}`)
-    let responseJson = await response.json()
-
-    console.log(responseJson)
-
-    console.log("fetch után")
-
-    rootElement.insertAdjacentHTML("beforeend", sectionElement(responseJson.title, responseJson.url, responseJson.copyright, responseJson.explanation))
-
-    console.log("insert után")
-
-}
-
-window.addEventListener("load", loadEvent)
- */
-
-// - - - - - - - - - - - - - - - - - - - - - - -
-
-const countriesElement = function (countryName, capitalName, continent, flag) {
+let countriesElement = function (countryName, capitalName, continent, flag) {
     return `
     <p>Name: </p>
     <h1>${countryName}</h1>
@@ -71,13 +22,39 @@ const countriesElement = function (countryName, capitalName, continent, flag) {
 const loadEvent = async function () {
     const rootElement = document.getElementById("root")
 
+
+    // - - - - - FETCH DATA - - - - - 
+
     const response = await fetch("https://restcountries.com/v3.1/all")
     const responseJson = await response.json()
-    console.log(responseJson)
+    // console.log(responseJson)
 
-    for (const countryData of responseJson) {
-        rootElement.insertAdjacentHTML("beforeend", countriesElement(countryData.name.common, countryData.capital, countryData.continents, countryData.flags.png))
-    }
+
+    // - - - - - BUTTON COMPONENT - - - - -
+
+    rootElement.insertAdjacentHTML("beforebegin", searchElement())
+
+    const searchButton = document.getElementById("search-country")
+    const searchBar = document.querySelector(".search-bar")
+
+    searchButton.addEventListener("click", function () {
+
+        if (searchBar.value === "") {
+
+            alert("Please type a country name")
+
+        } else {
+
+            for (const countryData of responseJson) {
+                if (searchBar.value.toUpperCase() === countryData.name.common.toUpperCase()) {
+                    rootElement.insertAdjacentHTML("afterbegin", countriesElement(countryData.name.common, countryData.capital, countryData.continents, countryData.flags.png))
+                }
+            }
+
+        }
+
+    })
+
 }
 
 
